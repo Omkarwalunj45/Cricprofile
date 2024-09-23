5f0aa315cf1cdbccc96ec716c72383b9702f2521
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-09-23 19:41:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title('Cricketer Profile Dashboard (Women) :cricketer:')
+st.title('Cricketer Profile Dashboard (Women) :cricket player:')
 st.header("ICC Women's T20WC 2024")
