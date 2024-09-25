f95fc5bf11ec8fad21a830c231aeba855e25620b
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-09-25 08:58:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,26 +3,5 @@ import pandas as pd
 
 player_data = pd.read_csv("Dataset/WT20I_Bat.csv")
 player_info = pd.read_csv("Dataset/squads.csv")
-# Create the Streamlit app
-st.set_page_config(page_title='Player Profile')
-# Load the player data from the Excel sheet
-player_data = pd.read_csv("Dataset/WT20I_Bat.csv")
-player_info = pd.read_csv("Dataset/squads.csv")
-
-# Add a sidebar for the player search
-st.sidebar.title('Player Search')
-search_term = st.sidebar.text_input('Enter player name:')
-
-# Filter the player data based on the search term
-filtered_players = player_data[player_data['batsman'].str.contains(search_term, case=False)]
-
-# Display the player profile
-st.title(player['batsman'])
-# st.subheader(f"{player['Age']} years old")
-# st.text(player_info['batting_hand'])
-# st.text(f"Bowling style: {player['Bowling Style']}")
-# st.text(f"Playing role: {player['Playing Role']}")
-# st.subheader('Career Performance')
-st.dataframe(player_data[['runs', 'ball', 'SR']])
-else:
-    st.write('No player found.')
+st.set_page_config(page_title='WT20I Performance Analysis Portal', layout='wide')
+st.title('WT20I Performance Analysis Portal')
