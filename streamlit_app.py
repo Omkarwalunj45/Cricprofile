79422b905326fc17572060619a01f914e56aea75
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-09-25 08:52:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+player_data = pd.read_csv("Dataset/WT20I_Bat.csv")
+player_info = pd.read_csv("Dataset/squads.csv")
 # Create the Streamlit app
 st.set_page_config(page_title='Player Profile')
 # Load the player data from the Excel sheet
@@ -15,14 +17,12 @@ search_term = st.sidebar.text_input('Enter player name:')
 filtered_players = player_data[player_data['batsman'].str.contains(search_term, case=False)]
 
 # Display the player profile
-if not filtered_players.empty:
-    for _, player in filtered_players.iterrows():
-        st.title(player['batsman'])
-        # st.subheader(f"{player['Age']} years old")
-        # st.text(player_info['batting_hand'])
-        # st.text(f"Bowling style: {player['Bowling Style']}")
-        # st.text(f"Playing role: {player['Playing Role']}")
-        # st.subheader('Career Performance')
-        st.dataframe(player_data[['runs', 'ball', 'SR']])
+st.title(player['batsman'])
+# st.subheader(f"{player['Age']} years old")
+# st.text(player_info['batting_hand'])
+# st.text(f"Bowling style: {player['Bowling Style']}")
+# st.text(f"Playing role: {player['Playing Role']}")
+# st.subheader('Career Performance')
+st.dataframe(player_data[['runs', 'ball', 'SR']])
 else:
     st.write('No player found.')
