cc58a28187ccee5ec9a21ec4a4712f787d396e43
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-09-25 09:00:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-
-player_data = pd.read_csv("Dataset/WT20I_Bat.csv")
-player_info = pd.read_csv("Dataset/squads.csv")
 st.set_page_config(page_title='WT20I Performance Analysis Portal', layout='wide')
 st.title('WT20I Performance Analysis Portal')
+
+pdf = pd.read_csv("Dataset/WT20I_Bat.csv")
+idf = pd.read_csv("Dataset/squads.csv")
