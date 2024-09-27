93054f273853a6acdec9af2824e83c488d506d0b
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-09-27 16:06:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.title('WT20I Performance Analysis Portal')
 # Load data
 pdf = pd.read_csv("Dataset/wt20i_compressed.csv",low_memory=False)
 idf = pd.read_csv("Dataset/lifesaver_bat.csv",low_memory=False)
-# info_df=pd.read_csv("Dataset/w_info_final.csv",low_memory=False)
+info_df=pd.read_csv("Dataset/w_info_final.csv",low_memory=False)
 bpdf=pd.read_csv("Dataset/wt20i_compressed.csv",low_memory=False)
 bidf=pd.read_csv("Dataset/lifesaver_bowl.csv",low_memory=False)
 def is_bowlers_wkt(player_dismissed,dismissal_kind):
